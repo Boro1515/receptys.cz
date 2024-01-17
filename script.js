4cc8f2e921375f595158a7b22cf4824073d735b2
Zdeněk Borovec <borovec.zde@seznam.cz>
2024-01-17 21:13:40 +0100

    replaced recipe title and update JS

--- script.js
+++ script.js
@@ -3,35 +3,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const navList = document.querySelector('nav ul');
 
     hamburgerMenu.addEventListener('click', function () {
-        // Přepínání viditelnosti navigačního menu
         navList.style.display = (navList.style.display === 'flex') ? 'none' : 'flex';
-    });
-
-    // Přidání event listeneru na změnu velikosti okna
-    window.addEventListener('resize', function () {
-        // Zobrazit navigační menu pouze pokud šířka obrazovky přesáhne 700px
-        if (window.innerWidth > 700) {
-            navList.style.display = 'flex';
-        } else {
-            navList.style.display = 'none';
-        }
-    });
-});
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburgerMenu = document.querySelector('.hamburger-menu');
-    const navList = document.querySelector('nav ul');
-
-    hamburgerMenu.addEventListener('click', function () {
         hamburgerMenu.classList.toggle('active');
-        navList.classList.toggle('active');
     });
 
     window.addEventListener('resize', function () {
         if (window.innerWidth > 700) {
             navList.style.display = 'flex';
-            hamburgerMenu.classList.remove('active'); // Přidáno: Zajistí, že ikona bude vypadat jako tři čárky
+            hamburgerMenu.classList.remove('active');
         } else {
             navList.style.display = 'none';
         }
